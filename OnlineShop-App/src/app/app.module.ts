d1217ebfe0742be82be5eefdbd55dc5dc60a2b3a
Michal_M <mmielczarek12@gmail.com>
2019-04-01 16:43:50 +0200

    fix dropdowns with bootstrap

--- OnlineShop-App/src/app/app.module.ts
+++ OnlineShop-App/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { BsDropdownModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -22,7 +23,8 @@ import { AlertifyService } from './_services/Alertify/AlertifyService.service';
       BrowserModule,
       HttpClientModule,
       FormsModule,
-      AppRoutingModule
+      AppRoutingModule,
+      BsDropdownModule.forRoot(),
    ],
    providers: [
       AlertifyService,
