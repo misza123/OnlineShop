ff2334b066f1db3d014d27ed2f6d55214c418c1b
Michal_M <mmielczarek12@gmail.com>
2019-04-01 16:22:10 +0200

    add alertify, font-awesome, bootstrap,bootswatch auth service in angular app

--- OnlineShop-App/src/app/app.module.ts
+++ OnlineShop-App/src/app/app.module.ts
@@ -1,18 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+
+import { AuthService } from './_services/Auth/Auth.service';
+import { AlertifyService } from './_services/Alertify/AlertifyService.service';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      NavComponent
+   ],
+   imports: [
+      BrowserModule,
+      HttpClientModule,
+      FormsModule,
+      AppRoutingModule
+   ],
+   providers: [
+      AlertifyService,
+      AuthService
+   ],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
