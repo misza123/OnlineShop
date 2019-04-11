43d81fb064f715e451536fe368192913b16ca4b9
Michal_M <mmielczarek12@gmail.com>
2019-04-11 14:53:44 +0200

    GetProductsAsync

--- OnlineShop.WebApi/Products/ProductController.cs
+++ OnlineShop.WebApi/Products/ProductController.cs
@@ -1,4 +1,6 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using OnlineShop.WebApi.DataAccess;
 
 namespace OnlineShop.WebApi.Products
 {
@@ -6,6 +8,24 @@ namespace OnlineShop.WebApi.Products
     [ApiController]
     public class ProductController : ControllerBase
     {
-        
+        private readonly IUnitOfWorkFactory _uowFactory;
+        private readonly IRepository<Product> _productRepository;
+
+        public ProductController(IUnitOfWorkFactory uowFactory, IRepository<Product> productRepository)
+        {
+            _uowFactory = uowFactory;
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProductsAsync()
+        {
+            using (var uow = _uowFactory.Create())
+            {
+                var products = await _productRepository.GetAllAsync();
+                await uow.CompleteAsync();
+                return Ok(products);
+            }
+        }
     }
 }
