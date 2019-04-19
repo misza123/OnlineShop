1d616024a5bbfb99559c5e6e0a91cc24506085e0
Michal_M <mmielczarek12@gmail.com>
2019-04-19 16:15:45 +0200

    add publicId to photo

--- OnlineShop.WebApi/Migrations/20190419134711_PublicId_at_Photo.Designer.cs
+++ OnlineShop.WebApi/Migrations/20190419134711_PublicId_at_Photo.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OnlineShop.WebApi.DataAccess;
 
 namespace OnlineShop.WebApi.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20190419134711_PublicId_at_Photo")]
+    partial class PublicId_at_Photo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ OnlineShop.WebApi/Migrations/20190419134711_PublicId_at_Photo.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnlineShop.WebApi.Migrations
+{
+    public partial class PublicId_at_Photo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            //TODO Fix and uncomment
+            // migrationBuilder.DropForeignKey(
+            //     name: "FK_Orders_Users_UserId",
+            //     table: "Orders");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PublicId",
+                table: "Photos",
+                nullable: true);
+
+            // migrationBuilder.AlterColumn<int>(
+            //     name: "UserId",
+            //     table: "Orders",
+            //     nullable: false,
+            //     oldClrType: typeof(int),
+            //     oldNullable: true);
+
+            // migrationBuilder.AddForeignKey(
+            //     name: "FK_Orders_Users_UserId",
+            //     table: "Orders",
+            //     column: "UserId",
+            //     principalTable: "Users",
+            //     principalColumn: "Id",
+            //     onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // migrationBuilder.DropForeignKey(
+            //     name: "FK_Orders_Users_UserId",
+            //     table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "PublicId",
+                table: "Photos");
+
+            // migrationBuilder.AlterColumn<int>(
+            //     name: "UserId",
+            //     table: "Orders",
+            //     nullable: true,
+            //     oldClrType: typeof(int));
+
+            // migrationBuilder.AddForeignKey(
+            //     name: "FK_Orders_Users_UserId",
+            //     table: "Orders",
+            //     column: "UserId",
+            //     principalTable: "Users",
+            //     principalColumn: "Id",
+            //     onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
